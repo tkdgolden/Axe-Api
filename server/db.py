c67dc785d06ef4a39b8d48f698fa4adf994ab6bc
Olivia Golden <olivia.golden.w@gmail.com>
2024-05-24 12:27:39 -0500

    db connect

--- server/db.py
+++ server/db.py
@@ -5,15 +5,17 @@ import os
 if 'POSTGRES_PASSWORD_FILE' in os.environ:
     with open(os.environ['POSTGRES_PASSWORD_FILE'], 'r') as f:
         password = f.read().strip()
+    host="db"
+    username="postgres"
 else:
     password = os.environ['POSTGRES_PASSWORD']
+    hostname = "dpg-cp8bbe21hbls739vnffg-a"
+    port = "5432"
+    username = "test_axe_user"
 
 def db_connect():
     global conn
-    """ DOCKER """
-    # conn = psycopg2.connect(host="db", user="postgres", password=password, database="test_axe")
-    """ RENDER """
-    conn = psycopg2.connect('DB_URL')
+    conn = psycopg2.connect(host=hostname, user=username, password=password, database="test_axe", port=port)
 
     global CUR
     CUR = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
